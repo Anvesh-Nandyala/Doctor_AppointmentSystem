9ce7c3cc3b2f2ec3977e088e12616320aac4cb59
Anvesh-Nandyala <anveshnandyala09830@gmail.com>
2025-07-01 01:13:12 +0530

    minor changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,34 @@
 import { useState } from 'react'
 import React from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Myprofile from './pages/Myprofile'
+import Myappointments from './pages/Myappointments'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Doctors from './pages/Doctors'
+import Appointment from './pages/appointments'
+import Navbar from './components/Navbar'
+
 
 function App() {
   
   return (
-    <div className='text-green-500'>
-      hola
+    <div className='mx-4 sm:mx-[10%]'>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/doctors' element={<Doctors/>} />
+        <Route path='/doctors/:speciality' element={<Doctors/>} />
+        <Route path='/login' element={<Login/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/my-profile' element={<Myprofile/>} />
+        <Route path='/my-appointments' element={<Myappointments/>} />
+        <Route path='/appointments/:docId' element={<Appointment/>} />
+      </Routes>
     </div>
   )
 }
