97d2731b42450be35fca6ccf8023220416579d68
Anvesh-Nandyala <anveshnandyala09830@gmail.com>
2025-06-30 22:22:57 +0530

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import React from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className='text-green-500'>
+      hola
+    </div>
+  )
+}
+
+export default App
