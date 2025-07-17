5b2d3ebee4b8caa27da91f165abab2b077d1b7e2
Anvesh-Nandyala <anveshnandyala09830@gmail.com>
2025-07-18 01:23:11 +0530

    final

--- admin/src/context/DoctorContext.jsx
+++ admin/src/context/DoctorContext.jsx
@@ -9,7 +9,7 @@ export const DoctorContext = createContext();
 
 const DoctorContextProvider = (props) =>{
 
-    const backendUrl= import.meta.env.VITE_BACKEND_URL;
+    const backendUrl= "https://daps-hglp.onrender.com";
 
     const [dToken ,setDToken] = useState(localStorage.getItem('dToken') ? localStorage.getItem('dToken') : null);
 
