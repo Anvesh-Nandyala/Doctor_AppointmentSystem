c0545e24bfb5e06eb6c9ed1be6f9cf659ead90cb
Anvesh-Nandyala <anveshnandyala09830@gmail.com>
2025-07-18 01:20:57 +0530

    final

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -6,7 +6,7 @@ import { toast } from "react-toastify";
 const AppContextProvider =(props)=>{
 
     const currencySymbol='$'
-    const backendUrl=import.meta.env.VITE_BACKEND_URL;
+    const backendUrl="https://daps-hglp.onrender.com";
 
     const [doctors, setDoctors] = useState([]);
     const [token,setToken] = useState(localStorage.getItem('token') ? localStorage.getItem('token'):false )
